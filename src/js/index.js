66faf903cb4988bd2fbf2a42e5d80b560d2c40a4
marinaldofilho <marinaldofilho1@gmail.com>
2022-11-02 15:05:49 -0300

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function () {
+
+        if (aba.classList.contains("selecionado")) {
+            return;
+        }
+
+        selecionarAba(aba)
+        mostrarInformacoesDaAba(aba)
+    });
+});
+
+function selecionarAba(aba) {
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+
+    aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba) {
+
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+
+    const idDoElementoDeInformacaoDaAba = `informacao-${aba.id}`
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacaoDaAba)
+    informacaoASerMostrada.classList.add("selecionado")
+}
